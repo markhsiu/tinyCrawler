59bb00412ba3264e4e30d5845caeb111b5701dbd
MarkHsiu <markhsiu@gmail.com>
2016-12-20 20:53:03 +0800

    去掉多于的jar

--- src/main/java/com/main/Liuxin.java
+++ src/main/java/com/main/Liuxin.java
@@ -6,8 +6,8 @@ import com.lizi.crawler.util.WebClientUtils;
 public class Liuxin {
 
 	private static final String url = 
-			"http://gsxt.gdgs.gov.cn//GSpublicity/GSpublicityList.html?service=entInfo&entNo=3c8f9781-014e-1000-e000-56d00a76b50b&regOrg=441920";
-	
+			"http://gsxt.gdgs.gov.cn/aiccips/GSpublicity/GSpublicityList.html?service=entInfo_i1hWfEEgMLQ7xAUni96soum8L3ko11c82v1+R9AVE8MYNDfrXVCs/2yxfT4dfkNy-7vusEl1hPU+qjV70QwcUXQ==";
+		
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebClientUtils.builder();
